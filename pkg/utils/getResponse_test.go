0ae61fea8dc1e19ebbb8755ad90b613792494f1c
rohandas-max <rohandasd0@gmail.com>
2022-02-16 13:36:25 +0530

    test added

---
+++ pkg/utils/getResponse_test.go
@@ -0,0 +1,88 @@
+package utils
+
+import (
+	"context"
+	"net/http"
+	"testing"
+)
+
+func TestGet(t *testing.T) {
+	type args struct {
+		ctx context.Context
+		url string
+	}
+	tests := []struct {
+		name string
+		args args
+
+		wantErr bool
+	}{
+		{
+			name: "fail(passing wrong url)",
+			args: args{
+				ctx: context.Background(),
+				url: "asdasdasd.com",
+			},
+
+			wantErr: true,
+		},
+		{
+			name: "pass(passing github api url)",
+			args: args{
+				ctx: context.Background(),
+				url: "http://api.github.com/",
+			},
+
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := Get(tt.args.ctx, tt.args.url)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Get() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
+
+func Test_checkStatus(t *testing.T) {
+	resp, _ := http.Get("http://api.github.com/")
+
+	type args struct {
+		h *http.Response
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "fail(passing nil http.response)",
+			args: args{
+				h: &http.Response{},
+			},
+			wantErr: true,
+		},
+		{
+			name: "pass",
+			args: args{
+				h: resp,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := checkStatus(tt.args.h)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("checkStatus() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+		})
+	}
+	defer resp.Body.Close()
+
+}
